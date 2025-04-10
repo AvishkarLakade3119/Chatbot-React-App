68a474153680cab0c80cbf59cd94afc3ea093328
AvishkarLakade3119 <90135361+AvishkarLakade3119@users.noreply.github.com>
2025-04-10 16:07:26 +0530

    Update Chatbox.jsx

--- src/Chatbox.jsx
+++ src/Chatbox.jsx
@@ -52,7 +52,7 @@ function Chatbox({ api_key }) {
   return (
     <div className={`chatbox ${mode ? 'light-mode' : 'dark-mode'}`}>
       <h2 style={{ textAlign: 'center', padding: '10px', color: mode ? '#333' : '#ddd' }}>
-        🚀 CI/CD Deployment Success — Shaiz 🚀
+        CI/CD Deployment Success — Avishkar 
       </h2>
 
       <header>
